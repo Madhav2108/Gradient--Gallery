8f9a0770a868e730231d18ca3f2a859b4289a9ff
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-18 20:47:26 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+/* Please ❤ this if you like it! */
+
+
 (function($) { "use strict";
 		
 	//Page cursors
@@ -28,7 +31,54 @@
     }
 
 	
+	//About page
 	
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	
+	//Contact page
+	
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	
+	//Travel portfolio page
+	
+	$(".travel").on('click', function () {
+		$("body").addClass("travel-on");
+	});
+	$(".travel-close").on('click', function () {
+		$("body").removeClass("travel-on");
+	});
+
+	
+	//Wildlife portfolio page
+	
+	$(".wildlife").on('click', function () {
+		$("body").addClass("wildlife-on");
+	});
+	$(".wildlife-close").on('click', function () {
+		$("body").removeClass("wildlife-on");
+	});
+
+	
+	//Nature portfolio page
+	
+	$(".nature").on('click', function () {
+		$("body").addClass("nature-on");
+	});
+	$(".nature-close").on('click', function () {
+		$("body").removeClass("nature-on");
+	});
 
 	
 })(jQuery);
